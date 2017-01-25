bb2e9e3439d6dda5c74f44697580a9eae8901ba8
Romario Huebra <romarioj2h@gmail.com>
2017-01-24 23:01:42 -0300

    Correcciones

--- js/play.js
+++ js/play.js
@@ -1,10 +1,6 @@
 var play = {
 
     preload: function () {
-        game.load.audio('Nivel1', 'assets/sounds/nivel1.mp3');
-        game.load.audio('AgarrarPieza', 'assets/sounds/agarrar.mp3');
-        game.load.audio('ErrorPieza', 'assets/sounds/error.mp3');
-        game.load.audio('OkPieza', 'assets/sounds/pegar.mp3');
 
         game.load.image('fondo', '/assets/img/fondoColor.png');
         game.load.image('cadenaGlow', '/assets/img/CadenaAdnGlow.png');
@@ -12,7 +8,15 @@ var play = {
         game.load.image('luces', '/assets/img/fndLz.png');
         game.load.image('particula', '/assets/img/particula.png');
 
-        game.load.text('nivel1', 'niveles/nivel1.txt');
+        game.load.audio('agarrar', 'assets/sounds/agarrar.mp3');
+        game.load.audio('error', 'assets/sounds/error.mp3');
+        game.load.audio('pegar', 'assets/sounds/pegar.mp3');
+        game.load.audio('level1', 'assets/sounds/nivel1.mp3');
+
+        //game.load.text('nivel1', 'niveles/nivel1.txt');
+
+       
+        this.nivel=[];
 
         for (var i = 0; i < 2; i++) {
 
@@ -21,6 +25,12 @@ var play = {
                 game.load.image('pSnCl' + i + j, '/assets/img/level1/' + i + j + '.png');
                 console.log('/assets/img/level1/' + i + j + '.png');
 
+               
+                pos=Math.floor((Math.random() * 2)); // entre 0 y 1
+                num=Math.floor((Math.random() * 14)); // entre 0 y 13
+
+                this.nivel.push(pos);
+                this.nivel.push(num);
 
             }
         }
@@ -30,21 +40,12 @@ var play = {
 
     create: function () {
 
-        this.Nivel1 = game.add.audio('Nivel1');
-        this.Nivel1.play();
-        this.Nivel1.loopFull();
-        this.AgarrarPieza = game.add.audio('AgarrarPieza');
-        this.ErrorPieza = game.add.audio('ErrorPieza');
-        this.OkPieza = game.add.audio('OkPieza');
 
         game.physics.startSystem(Phaser.Physics.ARCADE);
 
         game.add.image(0, 0, 'fondo');
         game.add.image(0, 0, 'luces');
-        game.add.image(0, 0, 'particula');
 
-
-        
 
         this.adn = game.add.tileSprite(0, game.world.centerY, 900, 189, 'cadena');
         this.adn.anchor.setTo(0, 0.5);
@@ -52,33 +53,44 @@ var play = {
         this.glow = game.add.tileSprite(0, game.world.centerY, 900, 333, 'cadenaGlow');
         this.glow.anchor.setTo(0, 0.5);
 
+        this.error = game.add.audio('error');
+        this.pegar = game.add.audio('pegar');
+        this.agarrar = game.add.audio('agarrar');
+        this.level1 = game.add.audio('level1');
+        this.level1.loopFull();
+        this.level1.play();
+
 
         this.sinColocarGroup = game.add.physicsGroup();
         this.colocadasGroup = game.add.physicsGroup();
 
 
-        this.nivel = game.cache.getText('nivel1').split(',');
+        //this.nivel = game.cache.getText('nivel1').split(',');
         console.log("Nivel: " + this.nivel);
 
 
 
-        var x = 10;
+        var x = 50;
         var y = 10;
-        var salto = 0;
+        var foo = 0;
+
 
         this.sinColocar = [];
         this.colocadas = [];
+
+        this.pz = [];
+        this.posX = [];
+        this.posY = [];
 
         for (i = 0; i < 14; i++) {
             for (j = 0; j < 2; j++) {
 
 
-                salto++;
                 indice = j * 14 + i;
 
-                if (salto == 15) {
+                if (foo == 14) {
                     y += 480;
-                    x = 40;
+                    x = 50;
                 }
 
                 this.sinColocar[indice] = this.sinColocarGroup.create(x += 50, y, 'pSnCl' + j + i);
@@ -87,19 +99,25 @@ var play = {
                 this.sinColocar[indice].inputEnabled = true;
                 this.sinColocar[indice].input.enableDrag(true);
 
+                this.pz.push('' + j + i);
+                this.posX.push(x);
+                this.posY.push(y);
+
 
                 this.sinColocar[indice].events.onDragStart.add(this.startDrag, this);
                 this.sinColocar[indice].events.onDragStop.add(this.stopDrag, this);
 
 
+                foo++;
 
 
-                //console.log("El random es: ", this.numero);
 
 
             }
 
         }
+
+        console.log("this.pz: ", this.pz);
 
         x = 650;
 
@@ -110,15 +128,15 @@ var play = {
             indice = i * 0.5;
 
             upOrDown = this.nivel[i];
-            if (upOrDown == 0) y = 300;
+            if (upOrDown == 0) y = 297;
 
             tipo = this.nivel[i + 1];
             this.colocadas[indice] = this.colocadasGroup.create(x += 225, y, 'pSnCl' + upOrDown + tipo);
-            this.colocadas[indice].tipo = tipo;
             this.colocadas[indice].upOrDown = upOrDown;
             this.colocadas[indice].inputEnabled = false;
             this.colocadas[indice].encastrada = -1;
             this.colocadas[indice].enable = true;
+            this.colocadas[indice].tipo = tipo;
 
 
         }
@@ -130,7 +148,7 @@ var play = {
 
     },
 
-    emisor: function(){
+    emisor: function () {
 
         emitter = game.add.emitter(game.world.centerX, 200, 200);
         emitter.width = 800;
@@ -153,16 +171,16 @@ var play = {
     },
 
     startDrag: function (pieza) {
-        this.AgarrarPieza.play();
 
-        this.posX = pieza.x;
-        this.posY = pieza.y;
-
-
+        this.agarrar.play();
     },
 
     stopDrag: function (pieza) {
-        game.physics.arcade.collide(pieza, this.colocadasGroup, this.collisionHandler, null, this);
+
+        if (!game.physics.arcade.collide(pieza, this.colocadasGroup, this.collisionHandler, null, this)) {
+            pieza.visible = false;
+            this.agregarPieza(pieza);
+        }
 
     },
     update: function () {
@@ -184,49 +202,60 @@ var play = {
             if (pCl.upOrDown == 0) pSnCl.y = pCl.y - 80;
             else pSnCl.y = pCl.y + 80;
 
-            //pSnCl.y = pCl.y + 80;
-            //pSnCl.body.velocity.x = pCl.body.velocity.x;
             pCl.encastrada = this.arrayObjectIndexOf(this.sinColocar, pSnCl);
 
-
-            pieza = this.sinColocarGroup.create(this.posX, this.posY, pSnCl.key);
-
-            console.log('Hola Mundo !!!');
-            console.log('pSnCl.key', pSnCl.key);
-            console.log('pieza', pieza);
-
-
-            ultimo = this.sinColocar.length;
-            this.sinColocar.push(pieza);
-            //this.sinColocar[this.sinColocar.length] = this.sinColocarGroup.create(50, 50, pSnCl.key);
-
-            //console.log('this.sinColocar[this.sinColocar.length-1]', this.sinColocar[this.sinColocar.length - 1]);
-
-
-
-            this.sinColocar[ultimo].tipo = pSnCl.tipo;
-            this.sinColocar[ultimo].upOrDown = pSnCl.upOrDown;
-            this.sinColocar[ultimo].inputEnabled = true;
-            this.sinColocar[ultimo].input.enableDrag(true);
-            this.sinColocar[ultimo].events.onDragStop.add(this.stopDrag, this);
-
+            this.agregarPieza(pSnCl);
 
             pSnCl.inputEnabled = false;
+
             game.camera.flash(0xffffff, 400);
-            this.OkPieza.play();
+
+            this.pegar.play();
+
             console.log("MATCH!!!  index: " + pCl.encastrada);
         } else {
             game.camera.flash(0xff0000, 400);
             game.camera.shake(0.02, 200);
 
             this.life--;
+
+            pSnCl.visible = false;
+            this.agregarPieza(pSnCl);
+
+            this.error.play();
+
             console.log("NO MATCH  life: " + this.life);
-            this.ErrorPieza.play();
-            if (this.life <= 0) {
+            if (this.life <= 0)
                 console.log("LOOOOSER");
-                this.final(false);
-            }
         }
+
+    },
+
+    agregarPieza: function (pzSnCl) {
+
+        var num;
+        var n = pzSnCl.key.replace('pSnCl', '');
+
+        for (var i = 0; i < this.pz.length; i++)
+            if (this.pz[i] == n) num = i;
+
+
+        pieza = this.sinColocarGroup.create(this.posX[num], this.posY[num], pzSnCl.key);
+
+        console.log('Hola Mundo !!!');
+        console.log('pzSnCl.key', pzSnCl.key);
+        console.log('pieza', pieza);
+
+
+        ultimo = this.sinColocar.length;
+        this.sinColocar.push(pieza);
+
+        this.sinColocar[ultimo].tipo = pzSnCl.tipo;
+        this.sinColocar[ultimo].upOrDown = pzSnCl.upOrDown;
+        this.sinColocar[ultimo].inputEnabled = true;
+        this.sinColocar[ultimo].input.enableDrag(true);
+        this.sinColocar[ultimo].events.onDragStop.add(this.stopDrag, this);
+        this.sinColocar[ultimo].events.onDragStart.add(this.startDrag, this);
 
     },
 
@@ -253,19 +282,16 @@ var play = {
                     }
                     this.colocadas[i].enable = false;
                     console.log("Life: " + this.life);
-                    if (this.life <= 0) {
+                    if (this.life <= 0)
                         console.log("LOOOOSER");
-                        this.final(false);
-                    }
+
 
                 }
             }
         }
         if (!this.colocadas[this.colocadas.length - 1].enable) {
-            if (this.life > 0) {
+            if (this.life > 0)
                 console.log("GANASTE!!!");
-                this.final(true);
-            }
         }
 
     },
@@ -275,16 +301,6 @@ var play = {
             if (myArray[i] === object) return i;
         }
         return -1;
-    },
-
-    final: function(gano) {
-        this.Nivel1.stop();
-        if(gano) {
-            game.state.start('animales', true, false, 8, true);
-        }
-        else {
-            game.state.start('animales', true, false, 1, false);
-        }
     }
 
 
